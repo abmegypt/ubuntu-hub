d644b46e85ad8acdbb39fea71b0b9c86db0146be
B.Shahin <34299580+bshahin@users.noreply.github.com>
2025-01-05 01:42:17 +0200

    Update setup-webuzo.sh

--- setup-webuzo.sh
+++ setup-webuzo.sh
@@ -10,8 +10,8 @@ sudo apt install fish -y
 chsh -s /usr/bin/fish
 echo "Fish shell installed and set as default."
 
-# Switch to fish (interactive session)
-exec fish
+# # Switch to fish (interactive session)
+# exec fish
 
 # 3. Install tmux
 sudo apt install tmux -y
@@ -25,7 +25,9 @@ tmux new-session -d -s mysession bash -c "
     exec bash
 "
 
-# Notify user of the created tmux session
-echo -e "\033[1;32mTmux session 'mysession' has been created. Use the following command to attach:\033[0m"
-echo -e "\033[1;34mtmux attach -t mysession\033[0m"
+# # Notify user of the created tmux session
+# echo -e "\033[1;32mTmux session 'mysession' has been created. Use the following command to attach:\033[0m"
+# echo -e "\033[1;34mtmux attach -t mysession\033[0m"
 
+# Attach to the tmux session directly
+tmux attach -t mysession
